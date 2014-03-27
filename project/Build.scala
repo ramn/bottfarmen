0abd449135c5435a5f12b8da4e7caa8d9a485637
ramn <github@rymdimperiet.org>
2014-03-27 20:48:05 +0100

    Add -Xlint:unchecked

--- project/Build.scala
+++ project/Build.scala
@@ -7,7 +7,8 @@ object Settings {
   lazy val common = Project.defaultSettings ++ Seq(
     version := "0.1",
     scalaVersion := "2.10.3",
-    scalacOptions ++= Seq("-deprecation")
+    scalacOptions ++= Seq("-deprecation"),
+    javacOptions ++= Seq("-Xlint:unchecked")
    )
 
   lazy val desktop = Settings.common ++ Seq (
