d320bb624b3a6ece895494df91c6a173fd923bf9
ramn <github@rymdimperiet.org>
2014-03-29 17:15:23 +0100

    Add Java example bot commander

---
+++ common/src/main/java/javaexample/JavaBotCommander.java
@@ -0,0 +1,23 @@
+package se.ramn.bottfarmen.javaexample;
+
+import java.util.List;
+import java.util.Collections;
+import se.ramn.bottfarmen.api.BotCommander;
+import se.ramn.bottfarmen.api.GameState;
+import se.ramn.bottfarmen.api.Command;
+import se.ramn.bottfarmen.api.Move;
+
+
+class JavaBotCommander implements BotCommander {
+  public JavaBotCommander() {
+  }
+
+  public String name() {
+    return "Java Commander";
+  }
+
+  public List<Command> update(GameState gameState) {
+    final Command command = new Move(1, "n");
+    return Collections.singletonList(command);
+  }
+}
