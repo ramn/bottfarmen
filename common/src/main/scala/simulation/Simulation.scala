07f6cfeb6b28ea963ae3640724f266437de80d9c
ramn <github@rymdimperiet.org>
2014-04-10 00:57:18 +0200

    Style

--- common/src/main/scala/simulation/Simulation.scala
+++ common/src/main/scala/simulation/Simulation.scala
@@ -1,8 +1,9 @@
 package se.ramn.bottfarmen.simulation
 
 import collection.immutable.Iterable
+
 import se.ramn.bottfarmen.api
-import impl.SimulationImpl
+import se.ramn.bottfarmen.simulation.impl.SimulationImpl
 import se.ramn.bottfarmen.simulation.entity.Bot
 import se.ramn.bottfarmen.simulation.entity.Base
 import se.ramn.bottfarmen.simulation.entity.BotCommander
