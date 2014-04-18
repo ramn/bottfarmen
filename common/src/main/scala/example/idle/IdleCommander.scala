96d147379b162858b37072ec6936f1b77ad18d28
ramn <github@rymdimperiet.org>
2014-04-18 18:30:48 +0200

    Add Idle commander
    Doesn't do anything

---
+++ common/src/main/scala/example/idle/IdleCommander.scala
@@ -0,0 +1,26 @@
+package se.ramn.bottfarmen.example.idle
+
+import collection.JavaConverters._
+import collection.immutable.Seq
+
+import se.ramn.bottfarmen.api
+import se.ramn.bottfarmen.api.Command
+import se.ramn.bottfarmen.api.Move
+import se.ramn.bottfarmen.api.BotCommander
+import se.ramn.bottfarmen.api.GameState
+import se.ramn.bottfarmen.example.BaseCommander
+import se.ramn.bottfarmen.example.BaseBot
+
+
+class Bot(var underlying: api.Bot) extends BaseBot
+
+
+class BaseDefenderCommander extends BaseCommander[Bot] {
+  val name = "Idle"
+
+  override def makeBot(serverSideBot: api.Bot) = new Bot(serverSideBot)
+
+  override def selectCommands(gameState: GameState): Seq[Command] = {
+    Seq.empty[Command]
+  }
+}
