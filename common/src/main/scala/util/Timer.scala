fa73a38cdf30129e53d46e73bc84579c94e624b0
ramn <github@rymdimperiet.org>
2014-04-20 18:06:05 +0200

    Refactor: Clarify method name

--- common/src/main/scala/util/Timer.scala
+++ common/src/main/scala/util/Timer.scala
@@ -5,8 +5,8 @@ object Timer extends Logging {
   def time[T](message: String, thunk: => T): T = {
     val start = System.nanoTime
     val result = thunk
-    val duration = (System.nanoTime - start) / 1E6
-    logger.debug(s"$message, duration: $duration ms")
+    val durationMs = (System.nanoTime - start) / 1E6
+    logger.debug(s"$message, duration: $durationMs ms")
     result
   }
 }
