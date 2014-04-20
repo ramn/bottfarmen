7b1524736017ae39774db7e681efbc2f0cd8df93
ramn <github@rymdimperiet.org>
2014-04-20 17:59:52 +0200

    Add util method: time
    Useful for timing how long time it takes to execute a chunk of code.

---
+++ common/src/main/scala/util/Timer.scala
@@ -0,0 +1,12 @@
+package se.ramn.bottfarmen.util
+
+
+object Timer extends Logging {
+  def time[T](message: String, thunk: => T): T = {
+    val start = System.nanoTime
+    val result = thunk
+    val duration = (System.nanoTime - start) / 1E6
+    logger.info(s"$message, duration: $duration ms")
+    result
+  }
+}
